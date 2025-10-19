9a660c34d8d9cc012f5e19290bb095e446a4b0da
Soumyajit Rout <soumyajitrout81@gmail.com>
2025-10-19 09:50:32 +0530

    List and Tuples

--- Ch3_Lists_and_Tuples/4th.py
+++ Ch3_Lists_and_Tuples/4th.py
@@ -25,9 +25,9 @@ list1 = ["m", "a", "a", "m"]
 copy_list1 = list1.copy()
 copy_list1.reverse()
 if(copy_list1==list1):
-    print("pal")
+    print("palindrome")
 else:
-    print("notPal")
+    print("notPalindrome")
 
 
 #------------------------
---
+++ Ch4_Dictionary_and_Set/1st.py
@@ -0,0 +1,10 @@
+info = {
+    "key" : "value",
+    "name" : "ApnaCollege",
+    "learning" : "coding",
+    "age" : 35,
+    "isAdult" : True,
+    "marks" : 94.4
+}
+
+print(info)
