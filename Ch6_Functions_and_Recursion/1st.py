b863111b133b2e4f95b9a52a3554d065d0f3d225
Soumyajit Rout <soumyajitrout81@gmail.com>
2025-10-31 00:44:34 +0530

    30.10.2025

---
+++ Ch6_Functions_and_Recursion/1st.py
@@ -0,0 +1,12 @@
+def calc_sum(a, b):
+    sum=a+b
+    print(sum)
+    return sum
+
+
+a = 5;
+b = 10;
+print(calc_sum(a, b));
+
+
+
---
+++ Ch6_Functions_and_Recursion/2nd.py
@@ -0,0 +1,12 @@
+cities = ["Kolkata", "Delhi", "Noida", "Chennai"]
+
+def print_len(list):
+    print(len(list))
+
+print_len(cities)
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+
+print_list(cities)
